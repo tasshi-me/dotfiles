b3eac151069eddb63d6f175664867dff5f855cef
Masaharu TASHIRO <masatsr.kit@gmail.com>
2020-08-16 04:01:36 +0900

    feat(zsh): apply XDG Base Directory

--- zsh/.zshrc.d/02-shell.zsh
+++ zsh/.zshrc.d/02-shell.zsh
@@ -4,6 +4,12 @@
 # editor
 VISUAL=/usr/bin/vim
 EDITOR=/usr/bin/vim
+
+# XDG Base Directory
+export XDG_CONFIG_HOME="${HOME}/.config"
+export XDG_CACHE_HOME="${HOME}/.cache"
+export XDG_DATA_HOME="${HOME}/.local/share"
+
 # handler
 function command_not_found_handler(){
   echo -e "m9(^Д ^)"
