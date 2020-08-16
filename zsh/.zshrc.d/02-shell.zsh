191e7b5e4ee55c144083655fdf790fbe89b70e63
Masaharu TASHIRO <masatsr.kit@gmail.com>
2020-08-16 16:23:48 +0900

    feat(zsh): save history file to XDG_DATA_HOME/zsh/history

--- zsh/.zshrc.d/02-shell.zsh
+++ zsh/.zshrc.d/02-shell.zsh
@@ -10,6 +10,10 @@ export XDG_CONFIG_HOME="${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_DATA_HOME="${HOME}/.local/share"
 
+# history
+mkdir -p ${XDG_DATA_HOME}/zsh
+export HISTFILE="${XDG_DATA_HOME}/zsh/history"
+
 # handler
 function command_not_found_handler(){
   echo -e "m9(^Д ^)"
